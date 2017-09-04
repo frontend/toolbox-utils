cdd187c49a75f332aa52e31132658f3b3694d5b0
Marc Friederich <marc@antistatique.net>
2017-09-04 22:03:07 +0200

    Update 2017-09-04T20:02:54.824Z

--- components/base.js
+++ components/base.js
@@ -1,8 +1,4 @@
 // You will use that file to import all your scripts
 // Ex: import gallery from './gallery'
 
-import svgIcons from '../icons/svg-icons';
-import nav from 'molecules/nav/nav';
-
-nav();
-svgIcons();
+import '../icons/svg-icons.js';
