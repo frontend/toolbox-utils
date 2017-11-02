db3ec039747d1693cb478e091e0a51ced4644fae
Yann Gouffon <github@yago-art.ch>
2017-11-02 17:03:04 +0100

    🔧 change wrong reader path

--- tasks/prepare.js
+++ tasks/prepare.js
@@ -4,7 +4,7 @@ const config = require('./config');
 const fs = require('fs-extra');
 const fetch = require('node-fetch');
 
-const rawgit = config.reader_path || 'http://localhost/toolbox-env/toolbox-reader/build';
+const rawgit = config.reader_path || 'https://rawgit.com/frontend/toolbox-reader/master/build';
 const dirs = ['atoms', 'molecules', 'organisms', 'pages'];
 
 const prepare = async (done) => {
